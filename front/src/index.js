ae0e795d79ff15a88d1758d8a5d6984d15cabe04
1509075677 <jchen386@buffalo.edu>
2022-03-21 01:38:15 -0400

    master

--- front/src/index.js
+++ front/src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'));
