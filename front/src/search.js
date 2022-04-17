5802add19720bd1daa87498eb17b5dcd8f63c626
1509075677 <60755066+1509075677@users.noreply.github.com>
2022-04-16 20:48:44 -0400

    Update search.js

--- front/src/search.js
+++ front/src/search.js
@@ -5,9 +5,9 @@ import Axios from 'axios'
 
 function SearchPage(){
     let navigate = useNavigate();
-    let username = localStorage.getItem("SearchUser");
-    let Birth = localStorage.getItem("SearchBirth");
-    let email = localStorage.getItem("SearchEmail");
+    let username = ""
+    let birth = ""
+    let email = ""
     const [input,setInput] = useState('')
     const searchUser =() =>{
         // http://localhost:3301/search
@@ -17,16 +17,11 @@ function SearchPage(){
         }).then((res)=>{
             if(res.data.message){
               alert("Username Doesn't Exsit");
-              localStorage.setItem("SearchUser",'');
-              localStorage.setItem("SearchBirth",'');
-              localStorage.setItem("SearchEmail",'');
-              localStorage.setItem("UserExist",'false');
             }
             else{
-                localStorage.setItem("SearchUser",res.data[0].username);
-                localStorage.setItem("SearchBirth",res.data[0].birthday);
-                localStorage.setItem("SearchEmail",res.data[0].email);
-                localStorage.setItem("UserExist",'true');
+                username=res.data[0].username;
+                birth=res.data[0].birthday;
+                email=res.data[0].email;
                 alert("Show User Profile");
             }
         });
@@ -41,14 +36,14 @@ function SearchPage(){
                 <img class="searchImage1" src="./images/searchPage/search1.jpg" alt="searchImage1"/>
                 <form class="InputSearchUser" >
                     <label for="username">Search For Username: <br></br></label>
-                    <input type="text" placeholder="Enter Username" onChange={(e)=>setInput(e.target.value)}/>
+                    <input type="text" placeholder="Enter Username" onChange={(e)=>{setInput(e.target.value)}}/>
                     <button class="SearchButton" onClick={searchUser}><b>Search</b></button>
                 </form>
                 <div class="UserSearchResult">
                     <p class="Username"><b>User Name:</b></p>
                     <div class="showUsername">{<p>{username}</p>}</div>
                     <p class="Birthday"><b>Birthday:</b></p>
-                    <div class="showBirthday">{<p>{Birth}</p>}</div>
+                    <div class="showBirthday">{<p>{birth}</p>}</div>
                     <p class="Email"><b>Email:</b></p>
                     <div class="showEmail">{<p>{email}</p>}</div>
                 </div>
