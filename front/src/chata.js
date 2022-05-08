97b8b4b43469c9c714f43effa4d346b5304e3bfd
1509075677 <60755066+1509075677@users.noreply.github.com>
2022-05-07 22:16:52 -0400

    change to school server link in achat #47

--- front/src/chata.js
+++ front/src/chata.js
@@ -2,7 +2,7 @@ import "./chat.css";
 import io from "socket.io-client";
 import Chat from "./Chat";
 
-const socket = io.connect("http://localhost:3303");
+const socket = io.connect("http://cheshire.cse.buffalo.edu:3303");
 
 function Chata() {
   const username = localStorage.getItem("random");
