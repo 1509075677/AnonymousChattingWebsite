dc4037f72dbd3236cf3927a0ee64adf94f882045
1509075677 <60755066+1509075677@users.noreply.github.com>
2022-05-07 23:08:06 -0400

    added back button feature #48

--- front/src/chati.js
+++ front/src/chati.js
@@ -1,16 +1,18 @@
 import "./chat.css";
 import io from "socket.io-client";
-import { useState } from "react";
 import Chat from "./Chat";
+import { useNavigate } from "react-router-dom";
 
 const socket = io.connect("http://cheshire.cse.buffalo.edu:3303");
 
 function Chati() {
   const username = localStorage.getItem("username");
   socket.emit("join_room", "Identified");
+  let navigate = useNavigate();
   return (
     <div>
       <Chat socket={socket} username={username} room={"Identified"} />
+      <button class="Back" onClick={()=>{navigate("/main")}}>BACK</button>
     </div>
   );
 }
